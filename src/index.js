ba977ce807dff65cc13228a67cc25b719ae59e72
Rinku <rinkurathore700@gmail.com>
2019-11-03 23:42:30 +0530

    Books.json file added for ajax

--- src/index.js
+++ src/index.js
@@ -2,87 +2,89 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class SearchEngine extends React.Component
+
+class BookSearchEngine extends React.Component
 {
     constructor(props)
     {
         super(props);
         this.state={
             query:"",
-            results: [],
+            books: [],
             status : "Searching For "
         }
         this.inputChanged=this.inputChanged.bind(this);
-        this.buttonClicked=this.buttonClicked.bind(this);
+       
     }
-
-    buttonClicked(event)
-    {
-        var that=this;
-        var temp=[]; 
-       let url="https://api.duckduckgo.com/?q="+this.state.query+"&format=json&pretty=1";
-       let request=new XMLHttpRequest();
-       request.open('GET',url);
-       request.send();
-       request.onreadystatechange=() =>{
-       if(request.readyState == 4 && request.status == 200)
-       {
-        this.setState({
-            status:"Result For "
-            
-        });
-           let data = JSON.parse(request.responseText);
-            for(let i=0 ;i < data.RelatedTopics.length; i++)
-            {
-                let Text= data.RelatedTopics[i].Text;
-               let result= document.createElement('a');
-               result.href=data.RelatedTopics[i].FirstURL;
-               result.innerText=Text;
-              temp.push(result); 
-            }
+  
+componentDidMount()
+{
+    
+    let req=new XMLHttpRequest();
+    req.open("GET","books.json");
+    req.send();
+    req.onreadystatechange=()=>{
+      if(req.readyState==4&& req.status==200)
+      {
+          var data = JSON.parse(req.responseText);
+         
+        for(let i =0 ;i<data.length;i++)
+        {
           
-            that.setState({
-                results :  temp
-             })
-       }
-       console.log(that.state.results);
-      
+            if(data[i].language=="English")
+            {
+                this.setState({        
+                    books:this.state.books.push(data[i])
+                })
+            }
+        }
+        this.state.books.forEach((book)=>{
+            lang.push(<option>{book.language}</option>)
+        })
+      }  
     }
+        
 }
+
     inputChanged(event)
     {
-        this.setState({
-            query:event.target.value
-            
-        });
+         
         
+       
     }
     render()
     {
+       
         return (
         <div className="container-fluid">
+           
             <div className="row">
                 <div className="col-md-4 offset-md-4">
-                <h2>Search Engine</h2>
+                <h1>BOOK SEARCH</h1>
+                <hr/>
+                <h5>Enter Language of book</h5>
                 <hr/>
                 <label>
-                    <input onChange={this.inputChanged} className="form-control"/>
+                    <select onChange={this.inputChanged} className="form-control">
+                       {lang}
+                        </select>
                </label>
-                       <button onClick={this.buttonClicked} className="btn btn-info">Search</button>
+               <br/>
+        <button  className="btn btn-info">Search</button>
                        
                        <hr/>
                        <h5>{this.state.status +this.state.query}</h5>
-                       <SearchResult  results={this.state.results}/>
+                      
                     </div>
                 
                 </div>
-        
+                <BookSearchResult  Books={this.state.books}/>
         </div>
         );
     }
 }
 
-class SearchResult extends React.Component
+class BookSearchResult extends React.Component
 {
     constructor(props)
     {
@@ -92,18 +94,23 @@ class SearchResult extends React.Component
    
     render()
     {
-        var resultList=[];
-        for(var i = 0 ; i< this.props.results.length ; i++)
-        {
-           resultList.push(<a href={this.props.results[i].href}><li>{this.props.results[i].innerText}</li></a>);
-        }
-        console.log(resultList);
+       
+        //console.log(this.props.Books);
         return (
-    <ul className="result">
-      {resultList}
-    </ul>
+   <div className="table-responsive">
+       <table className="table">
+           <thead>
+             <tr>
+               <th scope="col">S.No</th>
+               <th scope="col">Title</th>
+               <th scope="col">Country</th>
+               <th scope="col">Language</th>
+            </tr>
+         </thead>
+           </table>
+       </div>
         );
+        }
     }
-}
 
-ReactDOM.render(<SearchEngine/>,document.getElementById('root'));
+ReactDOM.render(<BookSearchEngine/>,document.getElementById('root'));
